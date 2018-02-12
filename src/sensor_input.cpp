5b3e3b7aa828dcd7c2f133f71a1a4be42cf22dea
Joel Quanbeck <jqquanbeck@gmail.com>
2018-02-11 19:55:05 -0600

    Added IR mapping function

--- src/sensor_input.cpp
+++ src/sensor_input.cpp
@@ -4,10 +4,11 @@
 #include "std_msgs/MultiArrayLayout.h"
 #include "std_msgs/MultiArrayDimension.h"
 
-#include "std_msgs/UInt16MultiArray.h"
+#include "std_msgs/Float32MultiArray.h"
 
 #include "mcp3008Spi.h"
 #include "sensors.h"
+#include "dn2DistIR.h"
 
 #include <sstream>
 
@@ -17,7 +18,7 @@ int main (int argc, char **argv)
 	
 	ros::NodeHandle n;
 	
-	ros::Publisher IR_raw_pub = n.advertise<std_msgs::UInt16MultiArray>("IR_raw", 1000);
+	ros::Publisher IR_raw_pub = n.advertise<std_msgs::Float32MultiArray>("IR_raw", 1000);
 	
 	ros::Rate loop_rate(100);
 
@@ -29,13 +30,13 @@ int main (int argc, char **argv)
 	for (int i = 0; i < 8; i++)
 		IRsensors[i].init(&adc0, i);
 	
-	std_msgs::UInt16MultiArray IRmsg;
+	std_msgs::Float32MultiArray IRmsg;
 	IRmsg.data.resize(8);
 	
 	while (ros::ok())
 	{
 		for (int i = 0; i < 8; i++)
-			IRmsg.data[i] = IRsensors[i].getValue();
+			IRmsg.data[i] = dn2DistIR(IRsensors[i].getValue());
 		
 		IR_raw_pub.publish(IRmsg);
 		
