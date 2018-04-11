72d5a3f65d56c15e83630943159719bcc209c8a5
Joel Quanbeck <jqquanbeck@gmail.com>
2018-04-11 11:30:04 -0500

    removed broken PID controller

--- srr_embedded/src/motor_control.cpp
+++ srr_embedded/src/motor_control.cpp
@@ -1,7 +1,6 @@
 #include "ros/ros.h"
 #include <pigpiod_if2.h>
 #include <sstream>
-#include "pid.h"
 
 #include "std_msgs/MultiArrayLayout.h"
 #include "std_msgs/MultiArrayDimension.h"
@@ -22,51 +21,26 @@
 const int MOTOR_QUANTITY = 6;
 const int MOTOR_PINS[MOTOR_QUANTITY] = { 22, 23, 24, 25, 26, 27 };
 
-const int COUNTS_PER_REV = 1440; // encoder counts per revolution
-
-// PID constants
-const double DT = 0.1; // loop interval time
-const double MAX = 100; // max adjustment to make
-const double MIN = -100; // min adjustment to make
-const double KP = 0.1; // proportional gain
-const double KD = 0.01; // derivative gain
-const double KI = 0.5; // integral gain
-
 class Motor
 {
 public:
 	void init(int pi, int pin);
-	void update(double encoderCount);
-	void setSetpoint(double setPoint) { /*_setPoint = setPoint;*/ set_PWM_dutycycle(_pi, _pin, setPoint); } // testing drivers
+	void setSpeed(int dutyCycle);
 	
 private:
-	PID _pid;
-	
 	int _pi;
 	int _pin;
-	int _setPoint; // desired rev/s
-	int _lastCount;
 };
 
 void Motor::init(int pi, int pin)
 {
 	_pi = pi;
-	
-	_pid.init(DT, MAX, MIN, KP, KD, KI);
-	
 	_pin = pin;
-	_setPoint = 0;
-	_lastCount = 0;
 }
 
-void Motor::update(double encoderCount)
+void Motor::setSpeed(int dutyCycle)
 {
-	int dif = encoderCount - _lastCount;
-	int pv = dif / COUNTS_PER_REV / DT; // current rev/s
-	
-	_lastCount = encoderCount;
-	
-	set_PWM_dutycycle(_pi, _pin, _pid.calculate(_setPoint, pv));
+	set_PWM_dutycycle(_pi, _pin, dutyCycle);
 }
 
 class Listener
@@ -76,7 +50,6 @@ public:
 	
 	void setLspeed(const std_msgs::Float32 msg);
 	void setRspeed(const std_msgs::Float32 msg);
-	void updateEncoders(const std_msgs::Int16MultiArray msg);
 
 private:	
 	Motor motors[MOTOR_QUANTITY];
@@ -93,24 +66,16 @@ Listener::Listener(int pi)
 
 void Listener::setLspeed(const std_msgs::Float32 msg)
 {
-	motors[0].setSetpoint(msg.data);
-	motors[2].setSetpoint(msg.data);
-	motors[4].setSetpoint(msg.data);
+	motors[0].setSpeed(msg.data);
+	motors[2].setSpeed(msg.data);
+	motors[4].setSpeed(msg.data);
 }
 
 void Listener::setRspeed(const std_msgs::Float32 msg)
 {
-	motors[1].setSetpoint(msg.data);
-	motors[3].setSetpoint(msg.data);
-	motors[5].setSetpoint(msg.data);
-}
-
-void Listener::updateEncoders(const std_msgs::Int16MultiArray msg)
-{
-	for (int i = 0; i < MOTOR_QUANTITY; ++i)
-	{
-		motors[i].update(msg.data[i]);
-	}
+	motors[1].setSpeed(msg.data);
+	motors[3].setSpeed(msg.data);
+	motors[5].setSpeed(msg.data);
 }
 
 int main (int argc, char **argv)
@@ -128,7 +93,6 @@ int main (int argc, char **argv)
 	
 	ros::Subscriber leftSub = n.subscribe("left_speed", 1000, &Listener::setLspeed, &listener);
 	ros::Subscriber rightSub = n.subscribe("right_speed", 1000, &Listener::setRspeed, &listener);
-	ros::Subscriber encoderSub = n.subscribe("encoder_count", 1000, &Listener::updateEncoders, &listener);
 	
 	ros::spin();
 	
