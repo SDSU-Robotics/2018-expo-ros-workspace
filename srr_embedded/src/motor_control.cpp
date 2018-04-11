d0de97dcfdb6fcb0dcca2793e93967a22c2f3178
Joel Quanbeck <jqquanbeck@gmail.com>
2018-04-11 17:44:50 -0500

    switched to individual motor speed topics

--- srr_embedded/src/motor_control.cpp
+++ srr_embedded/src/motor_control.cpp
@@ -2,11 +2,7 @@
 #include <pigpiod_if2.h>
 #include <sstream>
 
-#include "std_msgs/MultiArrayLayout.h"
-#include "std_msgs/MultiArrayDimension.h"
-
-#include "std_msgs/Float32.h" // left_speed, right_speed
-#include "std_msgs/Int16MultiArray.h" // encoder_count
+#include "std_msgs/Float64.h" // left_speed, right_speed
 
 
 /* Motor labeling convention:
@@ -25,7 +21,7 @@ class Motor
 {
 public:
 	void init(int pi, int pin);
-	void setSpeed(int dutyCycle);
+	void setSpeed(double speed);
 	
 private:
 	int _pi;
@@ -38,9 +34,9 @@ void Motor::init(int pi, int pin)
 	_pin = pin;
 }
 
-void Motor::setSpeed(int dutyCycle)
+void Motor::setSpeed(double speed) // speed -100 to +100
 {
-	set_PWM_dutycycle(_pi, _pin, dutyCycle);
+	set_PWM_dutycycle(_pi, _pin, (speed + 100) * 255 / 200);
 }
 
 class Listener
@@ -48,8 +44,12 @@ class Listener
 public:
 	Listener(int pi);
 	
-	void setLspeed(const std_msgs::Float32 msg);
-	void setRspeed(const std_msgs::Float32 msg);
+	void setM0speed(const std_msgs::Float64 msg) { motors[0].setSpeed(msg.data); }
+	void setM1speed(const std_msgs::Float64 msg) { motors[1].setSpeed(msg.data); }
+	void setM2speed(const std_msgs::Float64 msg) { motors[2].setSpeed(msg.data); }
+	void setM3speed(const std_msgs::Float64 msg) { motors[3].setSpeed(msg.data); }
+	void setM4speed(const std_msgs::Float64 msg) { motors[4].setSpeed(msg.data); }
+	void setM5speed(const std_msgs::Float64 msg) { motors[5].setSpeed(msg.data); }
 
 private:	
 	Motor motors[MOTOR_QUANTITY];
@@ -64,19 +64,6 @@ Listener::Listener(int pi)
 	}
 }
 
-void Listener::setLspeed(const std_msgs::Float32 msg)
-{
-	motors[0].setSpeed(msg.data);
-	motors[2].setSpeed(msg.data);
-	motors[4].setSpeed(msg.data);
-}
-
-void Listener::setRspeed(const std_msgs::Float32 msg)
-{
-	motors[1].setSpeed(msg.data);
-	motors[3].setSpeed(msg.data);
-	motors[5].setSpeed(msg.data);
-}
 
 int main (int argc, char **argv)
 {
@@ -91,8 +78,12 @@ int main (int argc, char **argv)
 	
 	Listener listener(pi);
 	
-	ros::Subscriber leftSub = n.subscribe("left_speed", 1000, &Listener::setLspeed, &listener);
-	ros::Subscriber rightSub = n.subscribe("right_speed", 1000, &Listener::setRspeed, &listener);
+	ros::Subscriber M0Sub = n.subscribe("m0_control_effort", 1000, &Listener::setM0speed, &listener);
+	ros::Subscriber M1Sub = n.subscribe("m1_control_effort", 1000, &Listener::setM1speed, &listener);
+	ros::Subscriber M2Sub = n.subscribe("m2_control_effort", 1000, &Listener::setM2speed, &listener);
+	ros::Subscriber M3Sub = n.subscribe("m3_control_effort", 1000, &Listener::setM3speed, &listener);
+	ros::Subscriber M4Sub = n.subscribe("m4_control_effort", 1000, &Listener::setM4speed, &listener);
+	ros::Subscriber M5Sub = n.subscribe("m5_control_effort", 1000, &Listener::setM5speed, &listener);
 	
 	ros::spin();
 	
