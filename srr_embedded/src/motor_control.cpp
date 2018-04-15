86a6dbee1ce8ad00d821eacb1377333c4b684301
Joel Quanbeck <jqquanbeck@gmail.com>
2018-04-15 03:44:11 -0500

    added arm control

--- srr_embedded/src/motor_control.cpp
+++ srr_embedded/src/motor_control.cpp
@@ -19,6 +19,10 @@ const int MOTOR_QUANTITY = 6;
 const int PWM_PINS[MOTOR_QUANTITY] = { 26, 18, 20, 21, 22, 23 };
 const int DIR_PINS[MOTOR_QUANTITY]={ 5, 6, 16, 19, 12, 13 };
 
+const int VERT_ACTUATOR_PIN = 24;
+const int HORZ_ACTUATOR_PIN = 25;
+const int CLAW_PIN = 27;
+
 class Motor
 {
 public:
@@ -26,33 +30,43 @@ public:
 	void setSpeed(double speed);
 	
 private:
-	int pi_;
-	int pwm_;
-	int dir_;
+	int _pi;
+	int _pwm;
+	int _dir;
 };
 
 void Motor::init(int pi, int pwm, int dir)
 {
-	pi_ = pi;
-	pwm_ = pwm;
-	dir_ = dir;
+	_pi = pi;
+	_pwm = pwm;
+	_dir = dir;
 }
 
 void Motor::setSpeed(double speed) // speed -1 to +1
 {
 	// set direction output
 	if (speed > 0)
-		gpio_write(pi_, dir_, 1);
-	else 
+		gpio_write(_pi, _dir, 1);
+	else
 	{
-		gpio_write(pi_, dir_, 0);
+		gpio_write(_pi, _dir, 0);
 		speed = -speed;
 	}
 
 	// set magnitude
-	set_PWM_dutycycle(pi_, pwm_, speed * 255.0);
-	 ROS_INFO("PWM Val: %lf\n",speed * 255.0);
+	set_PWM_dutycycle(_pi, _pwm, speed * 255.0);
+	ROS_INFO("PWM Val: %lf\n",speed * 255.0);
 }
+
+class Actuator
+{
+public:
+	void init(int pi, int pin) { _pi = pi; _pin = pin; }
+	void setPosition(double position) { set_PWM_dutycycle(_pi, _pin, position * 255.0); }
+private:
+	int _pi;
+	int _pin;
+};
 
 class Listener
 {
@@ -65,9 +79,15 @@ public:
 	void setM3speed(const std_msgs::Float64 msg) { motors[3].setSpeed(msg.data); }
 	void setM4speed(const std_msgs::Float64 msg) { motors[4].setSpeed(msg.data); }
 	void setM5speed(const std_msgs::Float64 msg) { motors[5].setSpeed(msg.data); }
+	void setHorzPos(const std_msgs::Float64 msg) { horzActuator.setPosition(msg.data); }
+	void setVertPos(const std_msgs::Float64 msg) { vertActuator.setPosition(msg.data); }
+	void setClawPos(const std_msgs::Float64 msg) { claw.setPosition(msg.data); }
 
-private:	
+private:
 	Motor motors[MOTOR_QUANTITY];
+	Actuator horzActuator;
+	Actuator vertActuator;
+	Actuator claw;
 };
 
 Listener::Listener(int pi)
@@ -77,6 +97,13 @@ Listener::Listener(int pi)
 	{
 		motors[i].init(pi, PWM_PINS[i], DIR_PINS[i]);
 	}
+
+	// initialize linear actuators
+	horzActuator.init(pi, HORZ_ACTUATOR_PIN);
+	vertActuator.init(pi, VERT_ACTUATOR_PIN);
+
+	// initialize claw
+	claw.init(pi, CLAW_PIN);
 }
 
 
@@ -87,7 +114,7 @@ int main (int argc, char **argv)
 	ros::NodeHandle n;
 	
 	// initialize pigpio
-	int pi = pigpio_start(NULL, NULL);	
+	int pi = pigpio_start(NULL, NULL);
 	if (pi < 0)
 		ROS_INFO("Error: %s\n", pigpio_error(pi));
 	
@@ -99,7 +126,10 @@ int main (int argc, char **argv)
 	ros::Subscriber M3Sub = n.subscribe("m3_speed_control_effort", 1000, &Listener::setM3speed, &listener);
 	ros::Subscriber M4Sub = n.subscribe("m4_speed_control_effort", 1000, &Listener::setM4speed, &listener);
 	ros::Subscriber M5Sub = n.subscribe("m5_speed_control_effort", 1000, &Listener::setM5speed, &listener);
-	
+	ros::Subscriber horzSub = n.subscribe("horz_actuator_pos", 1000, &Listener::setHorzPos, &listener);
+	ros::Subscriber vertSub = n.subscribe("vert_actuator_pos", 1000, &Listener::setVertPos, &listener);
+	ros::Subscriber clawSub = n.subscribe("claw_pos", 1000, &Listener::setClawPos, &listener);
+
 	ros::spin();
 	
 	return 0;
--- srr_embedded/src/teleop.cpp
+++ srr_embedded/src/teleop.cpp
@@ -4,27 +4,42 @@
 #include <sensor_msgs/Joy.h>
 #include <sstream>
 
+const double ACTUATOR_RATE = 0.001;
+
+const double STARTING_HORZ = 1.0;
+const double STARTING_VERT = 0.5;
+const double STARTING_CLAW = 0.0;
+
 class Listener
 {
 public:
 	void update(const sensor_msgs::Joy::ConstPtr& Joy);
 	void init(ros::Publisher *, ros::Publisher *, ros::Publisher *, ros::Publisher *, ros::Publisher *, ros::Publisher *);
+	void getButtonStates(bool buttonStates[]) {  for (int i = 0; i < 12; i++) buttonStates[i] = _buttonStates[i]; };
+	bool isTeleop() { return teleopMode; }
+
 private:
 	bool teleopMode = true;
 
-	ros::Publisher *m0_pub_;
-	ros::Publisher *m1_pub_;
-	ros::Publisher *m2_pub_;
-	ros::Publisher *m3_pub_;
-	ros::Publisher *m4_pub_;
-	ros::Publisher *m5_pub_;
+	ros::Publisher *_m0_pub;
+	ros::Publisher *_m1_pub;
+	ros::Publisher *_m2_pub;
+	ros::Publisher *_m3_pub;
+	ros::Publisher *_m4_pub;
+	ros::Publisher *_m5_pub;
 
 	std_msgs::Float64 mL_speed_setpoint_msg;
 	std_msgs::Float64 mR_speed_setpoint_msg;
+
+	bool _buttonStates[12] = { 0 };
 };
+
 
 void Listener::update(const sensor_msgs::Joy::ConstPtr& Joy)
 {
+	for (int i =0 ; i < 12; i++)
+		_buttonStates[i] = Joy->buttons[i];
+
 	if (Joy->buttons[9] == 1)
 	{
 		teleopMode = !teleopMode;
@@ -39,24 +54,24 @@ void Listener::update(const sensor_msgs::Joy::ConstPtr& Joy)
 		mL_speed_setpoint_msg.data = Joy->axes[1];
 		mR_speed_setpoint_msg.data = Joy->axes[3];
 
-		m0_pub_->publish(mL_speed_setpoint_msg);
-		m2_pub_->publish(mL_speed_setpoint_msg);
-		m4_pub_->publish(mL_speed_setpoint_msg);
-	
-		m1_pub_->publish(mR_speed_setpoint_msg);
-		m3_pub_->publish(mR_speed_setpoint_msg);
-		m5_pub_->publish(mR_speed_setpoint_msg);
+		_m0_pub->publish(mL_speed_setpoint_msg);
+		_m2_pub->publish(mL_speed_setpoint_msg);
+		_m4_pub->publish(mL_speed_setpoint_msg);
+
+		_m1_pub->publish(mR_speed_setpoint_msg);
+		_m3_pub->publish(mR_speed_setpoint_msg);
+		_m5_pub->publish(mR_speed_setpoint_msg);
 	}
 }
 
 void Listener::init(ros::Publisher *m0, ros::Publisher *m1, ros::Publisher *m2, ros::Publisher *m3, ros::Publisher *m4, ros::Publisher *m5)
 {
-	m0_pub_ = m0;
-	m1_pub_ = m1;
-	m2_pub_ = m2;
-	m3_pub_ = m3;
-	m4_pub_ = m4;
-	m5_pub_ = m5;
+	_m0_pub = m0;
+	_m1_pub = m1;
+	_m2_pub = m2;
+	_m3_pub = m3;
+	_m4_pub = m4;
+	_m5_pub = m5;
 }
 
 
@@ -66,7 +81,7 @@ int main (int argc, char **argv)
 	
 	ros::NodeHandle n;
 	
-	//ros::Rate loop_rate(20);
+	ros::Rate loop_rate(100);
 	Listener listener;
 
         ros::Publisher m0_pub = n.advertise<std_msgs::Float64>("m0_speed_control_effort", 1000);
@@ -75,15 +90,86 @@ int main (int argc, char **argv)
         ros::Publisher m3_pub = n.advertise<std_msgs::Float64>("m3_speed_control_effort", 1000);
         ros::Publisher m4_pub = n.advertise<std_msgs::Float64>("m4_speed_control_effort", 1000);
         ros::Publisher m5_pub = n.advertise<std_msgs::Float64>("m5_speed_control_effort", 1000);
+	
+	ros::Publisher horz_pub = n.advertise<std_msgs::Float64>("horz_actuator_pos", 1000);
+	ros::Publisher vert_pub = n.advertise<std_msgs::Float64>("vert_actuator_pos", 1000);
+	ros::Publisher claw_pub = n.advertise<std_msgs::Float64>("claw_pos", 1000);
 
 	listener.init(&m0_pub, &m1_pub, &m2_pub, &m3_pub, &m4_pub, &m5_pub);
 
 	ros::Subscriber joySub = n.subscribe("joy", 10, &Listener::update, &listener);
 	
+	std_msgs::Float64 horz_pos_msg;
+        std_msgs::Float64 vert_pos_msg;
+        std_msgs::Float64 claw_pos_msg;
+
+	double horzPos = STARTING_HORZ;
+	double vertPos = STARTING_VERT;
+	double clawPos = STARTING_CLAW;
+
 	while (ros::ok())
 	{
+		if (listener.isTeleop())
+		{
+			bool buttonStates[12];
+			listener.getButtonStates(buttonStates);
+
+			// if X is pressed
+			if (buttonStates[0])
+			{
+				// rotate base
+			}
+
+			// if B is pressed
+			if (buttonStates[2])
+			{
+				// rotate base
+			}
+
+			// if Y is pressed
+			if (buttonStates[3])
+			{
+				vertPos += ACTUATOR_RATE;
+				if (vertPos > 1)
+					vertPos = 1;
+			}
+
+			// if A is pressed
+			if (buttonStates[1])
+			{
+				vertPos -= ACTUATOR_RATE;
+				if (vertPos < 0)
+					vertPos = 0;
+			}
+
+			// if RB is pressed
+                        if (buttonStates[5])
+                        {
+                                horzPos += ACTUATOR_RATE;
+                                if (horzPos > 1)
+                                        horzPos = 1;
+                        }
+
+			// if RT is pressed
+                        if (buttonStates[7])
+                        {
+                                horzPos -= ACTUATOR_RATE;
+                                if (horzPos < 0)
+                                        horzPos = 0;
+                        }
+
+
+			horz_pos_msg.data = horzPos;
+			vert_pos_msg.data = vertPos;
+			claw_pos_msg.data = clawPos;
+
+			horz_pub.publish(horz_pos_msg);
+			vert_pub.publish(vert_pos_msg);
+			claw_pub.publish(claw_pos_msg);
+		}
+
 		ros::spinOnce();
-		//loop_rate.sleep();
+		loop_rate.sleep();
 	}
 	
 	return 0;
